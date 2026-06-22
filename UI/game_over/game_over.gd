5aefe72f39495b1ce22e1fa46dbdd0d00dc3985f
usynin78-web <usynin78@gmail.com>
2026-06-22 09:10:19 +0400

    Remove exit button; add room instances
    Remove the exit button and related logic from the Game Over scene and script. Swap the 'continue' and 'settings' button nodes in the main menu scene. Add PackedScene resources and instance several UI/prop scenes (damage, cake, checkpoint, cube) into loc/roomtest/room.tscn and remove their redundant entries from roomtest.tscn. Update node unique_ids and resource references accordingly. Also adjust project window resolution to 1720x720.

--- UI/game_over/game_over.gd
+++ UI/game_over/game_over.gd
@@ -2,16 +2,15 @@ extends Control
 
 @onready var start_button = $VBoxContainer/start
 @onready var menu_button = $VBoxContainer/menu
-@onready var exit_button = $VBoxContainer/exit
+
 
 func _ready():
  start_button.visible = false
  menu_button.visible = false
- exit_button.visible = false
 
  start_button.pressed.connect(_on_start_pressed)
  menu_button.pressed.connect(_on_menu_pressed)
- exit_button.pressed.connect(_on_exit_pressed)
+
 
 func _on_start_pressed():
  var tree = get_tree()
@@ -32,6 +31,3 @@ func _on_menu_pressed():
   tree.change_scene_to_file("res://UI/main menu/main menu.tscn")
  else:
   push_error("SceneTree не найден")
-
-func _on_exit_pressed():
- get_tree().quit()
